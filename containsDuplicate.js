7323d439006f5b2541b19dd25c72cb73c34bf3ac
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-05-26 13:30:39 -0700

    added comments

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,14 +1,19 @@
 function containsDuplicate(numbers) {
+  // Creating a new Set to store elements.
   const numberSet = new Set();
 
+  // Iterating through each number in the array.
   for (const number of numbers) {
+    // If the number is already in the Set, there is a duplicate.
     if (numberSet.has(number)) {
-      return true;
+      return true; // Found a duplicate, return true.
     }
 
+    // If the number is not in the Set, add it to the Set.
     numberSet.add(number);
   }
 
+  // If there are no duplicates present, return false.
   return false;
 }
 
--- validAnagram.js
+++ validAnagram.js
@@ -1,26 +1,35 @@
 function isAnagram(s, t) {
+  // Check if the lengths of the input strings are different.
   if (s.length !== t.length) {
     return false;
   }
 
+  // Create an empty object to store character frequencies.
   const map = {};
 
+  // Iterate over the characters in the first string 's'.
   for (let i = 0; i < s.length; i++) {
+    // If the character is not already in the 'map' object, initialize its frequency to 0.
     if (!map[s[i]]) {
       map[s[i]] = 0;
     }
 
+    // Increment the frequency of the character.
     map[s[i]]++;
   }
 
+  // Iterate over the characters in the second string 't'.
   for (let j = 0; j < t.length; j++) {
+    // If the character does not exist in the 'map' object, it means it's not an anagram.
     if (!map[t[j]]) {
       return false;
     }
 
+    // Decrement the frequency of the character.
     map[t[j]]--;
   }
 
+  // If all characters in both strings have the same frequencies, they are anagrams.
   return true;
 }
 
