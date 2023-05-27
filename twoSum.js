b583a1627f4107452ee063591a96c2092568761a
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-05-27 15:22:44 -0700

    modified twoSum

--- twoSum.js
+++ twoSum.js
@@ -4,11 +4,13 @@ function twoSum(nums, target) {
 
   // Iterate through each element in the nums array.
   for (let i = 0; i < nums.length; i++) {
+
     // Calculate the complement of the current number.
     const complement = target - nums[i];
 
     // Check if the complement exists in the indices Map.
     if (indices.has(complement)) {
+      
       // Return the indices of the two numbers that add up to the target.
       return [indices.get(complement), i];
     }
@@ -16,4 +18,7 @@ function twoSum(nums, target) {
     // Store the current number and its index in the indices Map.
     indices.set(nums[i], i);
   }
+
+  // Return an empty array in case of no solution.
+  return [];
 }
