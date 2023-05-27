66f801c9b7010ec395889b76ff1bc6e8c02d35b6
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-05-26 19:48:20 -0700

    added binarySearch time and space complexity

--- binarySearch.js
+++ binarySearch.js
@@ -26,3 +26,23 @@ function binarySearch(nums, target) {
   // Target not found, return -1.
   return -1;
 }
+
+/* Time Complexity
+
+The time complexity of the binary search algorithm implemented in the provided code is O(log n), where n is the number of elements in the input array nums. 
+
+This is because the algorithm repeatedly divides the search space in half, reducing the number of remaining elements to search by half in each iteration. 
+
+As a result, the search space is effectively halved at each step, leading to a logarithmic time complexity.
+
+*/
+
+/* Space Complexity
+
+The space complexity of the code is O(1), which means it requires constant space regardless of the size of the input array. 
+
+The only additional space used in the code is for the variables left, right, middle, and potentialMatch, which are all simple integer variables and do not depend on the size of the input array. 
+
+The algorithm does not use any additional data structures or recursion, resulting in a constant space complexity.
+
+*/
