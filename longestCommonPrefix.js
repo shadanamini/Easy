d4155e820bdc9481a7c5d0b11533590d9d5889af
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-05-28 19:08:52 -0700

    modified solution

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,20 +1,21 @@
 function longestCommonPrefix(strings) {
-    let first = strings[0]; 
+    // Set the initial common prefix as the first string in the array.
+    let prefix = strings[0];
 
     // Iterate through each string in the array.
     for (let string of strings) { 
 
         // Iterate through each character in the current string, starting from the last character.
-        for (let i = first.length - 1; i >= 0; i--) { 
+        for (let i = prefix.length - 1; i >= 0; i--) { 
 
             // If the character at index (i) is not equal in the current string, update the common prefix.
-            if (first[i] !== string[i]) { 
+            if (prefix[i] !== string[i]) { 
                 // Remove characters from index (i) onwards from the common prefix.
-                first = first.slice(0, i); 
+                prefix = prefix.slice(0, i); 
             }
         }
     }
 
     // Return the final common prefix.
-    return first; 
+    return prefix; 
 }
