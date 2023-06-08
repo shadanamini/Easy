46307977183cc1e82a1f259ccedbe0a9e3687c31
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-08 13:25:09 -0700

    corrected solution

--- Longest Common Prefix.js
+++ Longest Common Prefix.js
@@ -1,11 +1,10 @@
 const longestCommonPrefix = function (strings) {
-    const prefix = strings[0];
+    let prefix = strings[0];
 
     for (const string of strings) {
         for (let i = prefix.length - 1; i >= 0; i--) {
             if (prefix[i] !== string[i]) {
-                const updatedPrefix = prefix.slice(0, i);
-                prefix = updatedPrefix;
+                prefix = prefix.slice(0, i);
             }
         }
     }
