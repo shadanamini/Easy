43d8b88f7cd4c6ae52dc7919ce9bdfb1147842f6
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-07 21:46:02 -0700

    formatting

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,21 +1,13 @@
-function isSubsequence(s, t) {
-    // Create pointers for string 's' and string 't'.
-    let sPointer = 0; 
-    let tPointer = 0; 
-  
+var isSubsequence = function(s, t) {
+    let sPointer = 0;
+    let tPointer = 0;
+
     while (sPointer < s.length && tPointer < t.length) {
-      // Continue until we reach the end of either 's' or 't'.
-  
-      if (s[sPointer] === t[tPointer]) {
-        // If the current characters match, move to the next character in 's'.
-        sPointer++; 
-      }
-  
-      // If the characters do not match, move to the next character in 't'.
-      tPointer++; 
+        if (s[sPointer] === t[tPointer]) {
+            sPointer += 1;
+        }
+        tPointer += 1;
     }
-  
-    // Return true if we reached the end of 's', false otherwise.
-    return sPointer === s.length; 
-}
-  
+
+    return sPointer === s.length;
+};
