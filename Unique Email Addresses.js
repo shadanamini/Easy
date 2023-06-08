77fd764cf258ad5cdc92d5a9b35789a526ff93dd
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-07 22:22:28 -0700

    modified uniqueEmailAddresses

---
+++ Unique Email Addresses.js
@@ -0,0 +1,13 @@
+var numUniqueEmails = function (emails) {
+  const set = new Set();
+
+  for (let email of emails) {
+    let [local, domain] = email.split('@');
+
+    local = local.replace(/(\.)|(\+.*)/g, '');
+
+    set.add(local + '@' + domain);
+  }
+
+  return set.size;
+};
