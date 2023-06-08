3a9c878daeead19400da99959f16fd7103f946f1
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-07 21:39:47 -0700

    modified validAnagram

--- validAnagram.js
+++ validAnagram.js
@@ -1,36 +1,23 @@
-function isAnagram(s, t) {
-  // Check if the lengths of the two strings are different.
-  if (s.length !== t.length) {
-    return false;
-  }
-
-  // Create an empty object to store the character frequency for string 's'.
-  const map = {};
-
-  // Iterate through string 's' and populate the character frequency map.
-  for (let i = 0; i < s.length; i++) {
-
-    // If the character is not present in the map, initialize the frequency to 0.
-    if (!map[s[i]]) {
-      map[s[i]] = 0;
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
     }
 
-    // Increment the frequency of the character in the map.
-    map[s[i]]++;
-  }
+    const map = {};
 
-  // Iterate through string 't'.
-  for (let j = 0; j < t.length; j++) {
-
-    // If the character is not present in the map or the frequency is 0, the strings are not anagrams.
-    if (!map[t[j]]) {
-      return false;
+    for (let i = 0; i < s.length; i++) {
+        if (!map[s[i]]) {
+            map[s[i]] = 0;
+        }
+        map[s[i]] += 1;
     }
 
-    // Decrement the frequency of the character in the map.
-    map[t[j]]--;
-  }
+    for (let j = 0; j < t.length; j++) {
+        if (!map[t[j]]) {
+            return false;
+        }
+        map[t[j]] -= 1;
+    }
 
-  // If both loops complete without finding any inconsistencies, the strings are anagrams.
-  return true;
-}
+    return true;
+};
