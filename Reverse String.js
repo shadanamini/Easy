bb320a35bb4255c8b6fdaea853669b7b15cb7825
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-08 14:01:28 -0700

    completed reverseString

---
+++ Reverse String.js
@@ -0,0 +1,15 @@
+const reverseString = function (string) {
+    let start = 0;
+    let end = string.length - 1;
+
+    while (start < end) {
+        const temp = string[start];
+        string[start] = string[end];
+        string[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    return string;
+};
