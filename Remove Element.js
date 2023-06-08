408b681db07190ca357fcf36624e02c76d39cd15
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-08 11:22:05 -0700

    completed removeElement

---
+++ Remove Element.js
@@ -0,0 +1,8 @@
+var removeElement = function (numbers, value) {
+    for (let i = numbers.length - 1; i >= 0; i--) {
+        if (numbers[i] === value) {
+            numbers.splice(i, 1);
+        }
+    }
+    return numbers.length;
+};
