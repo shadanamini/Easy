7b760bdf9270fe2fc4e7609602dbb39833d082ca
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-07 22:01:45 -0700

    modified lengthOfLastWord

--- lengthLastWord.js
+++ lengthLastWord.js
@@ -1,12 +1,9 @@
-function lengthOfLastWord(s) {
-    // Remove leading/trailing spaces and split the string into an array of words.
-    const words = s.trim().split(' ');
+var lengthOfLastWord = function (string) {
+    const words = string.trim().split(' ');
 
-    // Return 0 if there are no words.
     if (words.length === 0) {
         return 0;
     }
 
-    // Return the length of the last word.
     return words[words.length - 1].length;
-}
+};
