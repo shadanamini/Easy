f4c6cfc9e51ff2f633f09f2d08118ddd8538ec3e
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-08 12:38:25 -0700

    refactored with new function declaration

--- Concatenation Of Array.js
+++ Concatenation Of Array.js
@@ -1,3 +1,9 @@
-var getConcatenation = function (numbers) {
-    return numbers.concat(numbers);
+const getConcatenation = function (numbers) {
+    let result = [];
+
+    for (let i = 0; i < numbers.length * 2; i++) {
+        result.push(numbers[i % numbers.length]);
+    }
+    
+    return result;
 };
