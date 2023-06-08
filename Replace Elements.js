3c377cb4e3e3a0b246fe2279f2ba3f6314f286c5
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-08 11:26:37 -0700

    completed replaceElements

---
+++ Replace Elements.js
@@ -0,0 +1,18 @@
+var replaceElements = function (array) {
+    let maximum = array[array.length - 1];
+
+    array[array.length - 1] = -1;
+
+    for (let i = array.length - 2; i >= 0; i--) {
+        let current = array[i];
+        
+        array[i] = maximum;
+
+        if (current > maximum) {
+            maximum = current;
+        }
+
+    }
+
+    return array;
+};
