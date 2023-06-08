e8964657e9e50fc1ee4ef9da5a33207fc95cc844
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-07 22:12:38 -0700

    modified code

--- Valid Parentheses.js
+++ Valid Parentheses.js
@@ -1,30 +1,16 @@
-function isValid(string) {
-    // Create a mapping of opening brackets to their corresponding closing brackets.
+var isValid = function (string) {
     const map = {'(' : ')', '{' : '}', '[' : ']'};
-    
-    // Create an empty stack to store opening brackets.
     const stack = [];
 
-    // Iterate through each character in the input string.
     for (let character of string) {
-
-        // If the character is an opening bracket, push its corresponding closing bracket onto the stack.
         if (map[character]) {
             stack.push(map[character]);
-        }
-
-        // If character is a closing bracket and matches the top of the stack, pop the top element from the stack.
-        else if (stack.length > 0 && stack[stack.length - 1] === character) {
+        } else if (stack.length > 0 && stack[stack.length - 1] === character) {
             stack.pop();
-        }
-
-        // If character is a closing bracket but doesn't match the top of the stack or if not a bracket at all, return false.
-        else {
+        } else {
             return false;
         }
-
     }
 
-    // If the stack is empty, return true.
     return !stack.length;
-}
+};
