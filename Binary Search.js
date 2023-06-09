de0921669b5163e6d83c8b8a02a70cf6f82aeee8
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-09 09:17:28 -0700

    formatting

--- Binary Search.js
+++ Binary Search.js
@@ -1,11 +1,9 @@
 const search = function (nums, target) {
     let left = 0;
-    
     let right = nums.length - 1;
 
     while (left <= right) {
         const middle = Math.floor((left + right) / 2);
-
         const potentialMatch = nums[middle];
 
         if (potentialMatch === target) {
