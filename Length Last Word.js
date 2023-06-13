499ba632e5656975e1a05671e6727c7158aaea27
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-13 14:07:53 -0700

    Solution

--- Length Last Word.js
+++ Length Last Word.js
@@ -1,7 +1,9 @@
 const lengthOfLastWord = (string) => {
     const words = string.trim().split(' ');
+    
     if (words.length === 0) {
         return 0;
     }
+
     return words[words.length - 1].length;
 };
