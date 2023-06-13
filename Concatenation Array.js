4320cca133d26c529bb72573aa41e58b31ffb481
Shadan Amini <shadanamini@MORTEZAs-MacBook-Pro.local>
2023-06-13 14:17:40 -0700

    Faster Solution

--- Concatenation Array.js
+++ Concatenation Array.js
@@ -1,9 +1,3 @@
 const getConcatenation = (numbers) => {
-    let result = [];
-    
-    for (let i = 0; i < numbers.length * 2; i++) {
-        result.push(numbers[i % numbers.length]);
-    }
-
-    return result;
+    return numbers.concat(numbers);
 };
